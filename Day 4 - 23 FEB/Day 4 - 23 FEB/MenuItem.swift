64c0f020e1c2363d6f7173756b91b04e64632f26
Gurpreet Singh Bhachu <35268356+gurpreetbhachu@users.noreply.github.com>
2018-02-23 12:43:39 -0500

    Add files via upload

---
+++ Day 4 - 23 FEB/Day 4 - 23 FEB/MenuItem.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItem.swift
+//  Day 4 - 23 FEB
+//
+//  Created by MacStudent on 2018-02-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class MenuItem
+{
+    let names: [String] = ["Pizza", "Burger", "Pasta", "Noodles", "Daal Makhni", "Mutter Paneer", "Naan", "Tandoori Roti"]
+    
+    let prices:[Double] = [4.0, 2.0, 5.15, 15.15, 5.50, 1.00, 2.65, 2.0]
+    
+    let specials:[Bool] = [false, true, false, false, false, false, true, false]
+}
